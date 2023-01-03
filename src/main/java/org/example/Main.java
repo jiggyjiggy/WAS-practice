2b74efc048a2f4102833289ee2d41168756e545c
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-01-03 14:31:42 +0900

    feat: Paste my calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+package org.example;
+
+// GET /calculate?operand1=11&operator=*&operand2=55
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
